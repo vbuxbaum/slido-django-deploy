1fc45927859987e7693fcd66734e0414d0659d7e
Vitor Buxbaum <vitorbuxbaum@gmail.com>
2023-12-12 17:37:55 -0300

    tentando corrigir instalação do mysqlclient

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-export MYSQLCLIENT_CFLAGS=`pkg-config mysqlclient --cflags` && export MYSQLCLIENT_LDFLAGS=`pkg-config mysqlclient --libs` && apt install -y python3-dev default-libmysqlclient-dev build-essential pkg-config && pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput
+echo "Install dependencies"
+export MYSQLCLIENT_CFLAGS=$(pkg-config mysqlclient --cflags) && export MYSQLCLIENT_LDFLAGS=$(pkg-config mysqlclient --libs) && apt install -y python3-dev default-libmysqlclient-dev build-essential pkg-config && pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput
