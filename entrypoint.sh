d91931157ad391c383e09f19e452627638d22ba9
Vitor Buxbaum <vitorbuxbaum@gmail.com>
2023-12-12 17:35:35 -0300

    buildCommand: extrai para entripoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export MYSQLCLIENT_CFLAGS=`pkg-config mysqlclient --cflags` && export MYSQLCLIENT_LDFLAGS=`pkg-config mysqlclient --libs` && apt install -y python3-dev default-libmysqlclient-dev build-essential pkg-config && pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput
